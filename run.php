d5dde0716d4edc6099938f0479a23c02dd512736
nee48 <34019257+nee48@users.noreply.github.com>
2017-11-29 01:01:18 +0700

    Add files via upload

---
+++ run.php
@@ -0,0 +1,21 @@
+<?php
+include 'func.php';
+
+/*
+    https://github.com/nee48/BomSmsPhD
+    Made by Handika Pratama
+*/
+
+$init = new Bom();
+
+//Eksekusi Call/Sms Boomber (Limit 3x/Jam)
+$init->type = 2; //Type 2 untuk telpon, Type 1 untuk sms
+$init->no = ""; //Nomer Hp tujuan
+
+if ($init->type == 1) {
+	for ($i=0; $i < 2; $i++) { 
+	    $init->Verif($init->no,$init->type);
+	}
+}elseif ($init->type == 2) {
+	 $init->Verif($init->no,$init->type);
+}
